7a6f1f539d79fcb57b2da3f7d18ec926f1dae9f7
EvanstonLaw <kaluoshilong@qq.com>
2026-06-20 21:33:10 +0800

    Feat(Settings): 更新设置模式定义

--- src/settings/schema.ts
+++ src/settings/schema.ts
@@ -1,5 +1,6 @@
 export const SITE_RULES_URL = '__SITE_RULES_RAW_URL__';
 export const TEXT_RULES_URL = '__TEXT_RULES_RAW_URL__';
+export const S2T_RULES_URL = '__S2T_RULES_RAW_URL__';
 
 export const KEYS = {
   siteRulesUrl: 'siteRulesUrl',
@@ -8,7 +9,12 @@ export const KEYS = {
   textRulesUrl: 'textRulesUrl',
   textRulesCache: 'textRulesCache',
   textRulesCacheUpdatedAt: 'textRulesCacheUpdatedAt',
+  s2tRulesUrl: 's2tRulesUrl',
+  s2tRulesCache: 's2tRulesCache',
+  s2tRulesCacheUpdatedAt: 's2tRulesCacheUpdatedAt',
   customSiteRules: 'customSiteRules',
   customReplaceRules: 'customReplaceRules',
   enabledTextRuleGroups: 'enabledTextRuleGroups',
+  convertToTraditional: 'convertToTraditional',
+  splitContent: 'splitContent',
 } as const;
